986b6d273cb11ae1a8c2916435ac1d6b7e5672f4
kinar aurasae <nanamika1230@gmail.com>
2025-12-20 14:23:35 +0700

    .env example update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,16 +57,23 @@
             <span class="text-gray-700 font-medium text-sm">Google</span>
         </a>
         <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                href="{{ route('register') }}">
+                {{ __('Dont have an account? Register now!') }}
+            </a>
+        </div>
+        <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                     href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+
     </form>
 </x-guest-layout>
